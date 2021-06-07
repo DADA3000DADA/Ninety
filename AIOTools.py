ed46fcbd972e041815541af2d0add52b953a0e6d
DADA3000DADA <84926336+DADA3000DADA@users.noreply.github.com>
2021-06-07 12:28:48 +0300

    Update AIOTools.py

--- AIOTools.py
+++ AIOTools.py
@@ -1,7 +1,7 @@
 import time
 print("Загрузка")
 import os
-os.system("pkg update upgrade > /dev/null 2>&1 && pkg install python git > /dev/null 2>&1 && pip install requests html5lib bs4 phonenumbers argparse urllib3 colorama > /dev/null 2>&1")
+os.system("pkg update > /dev/null 2>&1 && pkg upgrade > /dev/null 2>&1 && pkg install python git > /dev/null 2>&1 && pip install requests html5lib bs4 phonenumbers argparse urllib3 > /dev/null 2>&1")
 os.system("pip install db0mb3r > /dev/null 2>&1")
 os.system("pip install colorama > /dev/null 2>&1")
 from colorama import Fore,Back,Style
@@ -27,5 +27,5 @@ elif dox=="2":
 elif dox=="3":
   quit()
 elif dox=="4":
-  os.system("cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety && rm -rf temp > dev/null 2>&1")
+  os.system("cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety > /dev/null 2>&1 && rm -rf temp && cd Ninety && python3 AIOTools.py")
   quit()
