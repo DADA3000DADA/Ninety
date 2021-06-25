1e5b13ee04ecaa6c9a1f3eaa2a05b8590d83f1b7
DADA3000DADA <84926336+DADA3000DADA@users.noreply.github.com>
2021-06-25 10:35:51 +0300

    Update AIOTools.py

--- AIOTools.py
+++ AIOTools.py
@@ -5,9 +5,9 @@ os.system("clear")
 print()
 print("Загрузка")
 print("Первая загрузка может длиться больше 5 минут! Время загрузки зависит от вашей скорости интернета")
-os.system("pkg update && pkg upgrade > dev/null 2>&1 && pkg install python git > dev/null 2>&1 && pip install requests html5lib bs4 phonenumbers argparse urllib3 colorama > dev/null 2>&1")
+os.system("pkg update && pkg upgrade > /dev/null 2>&1 && pkg install python git > /dev/null 2>&1 && pip install requests html5lib bs4 phonenumbers argparse urllib3 colorama > /dev/null 2>&1")
 os.system("n")
-os.system("pip install db0mb3r")
+os.system("pip install db0mb3r > /dev/null 2>&1")
 from colorama import Fore, Back, Style
 print(Fore.GREEN + "Загружено!")
 time.sleep(14)
@@ -33,6 +33,5 @@ elif dox == "2":
 elif dox == "3":
     quit()
 elif dox == "4":
-    os.system(
-        "cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety && rm -rf temp > dev/null 2>&1")
+    os.system("cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety > /dev/null 2>&1 && rm -rf temp && cd Ninety && python3 AIOTools.py")
     quit()
