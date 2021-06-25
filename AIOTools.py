2dac69a24633f9e06661bed902672dd213eb934a
DADA3000DADA <84926336+DADA3000DADA@users.noreply.github.com>
2021-06-25 10:12:56 +0300

    Update AIOTools.py

--- AIOTools.py
+++ AIOTools.py
@@ -1,35 +1,58 @@
 import time
-print("\nЗагрузка")
-import os
-os.system("pkg update > /dev/null 2>&1 && pkg upgrade > /dev/null 2>&1 && pkg install python git > /dev/null 2>&1 && pip install requests html5lib bs4 phonenumbers argparse urllib3 > /dev/null 2>&1")
-os.system("pip install db0mb3r > /dev/null 2>&1")
-os.system("pip install colorama > /dev/null 2>&1")
-from colorama import Fore,Back,Style
-print(Fore.GREEN + "Загружено!")
+
+print("Приветствую! Чтобы загрузить инструмент Хакера тебе нужно скачать файлы! Приступим")
+kko = input("Чтобы начать загрузку нажмите Enter! : ")
 time.sleep(2)
-kkr=input(Fore.RESET + "Нажмите Enter для продолжения")
+print("Разрешение получено!")
+time.sleep(1)
+print("Подключение серверов!")
+time.sleep(7)
+kkq = input("Выключите WiFi чтобы произошло подключение, затем нажмите Enter : ")
+time.sleep(2)
+print("Подключаем сервера Kali")
+time.sleep(4)
+print("Подключили!")
+time.sleep(1)
+print("Перезагружаем Termux")
+time.sleep(2)
+kkw = input("Нажмите Enter для перезагрузки Termux кэша")
+time.sleep(2)
+print("Перезагружаем")
+time.sleep(4)
+print("Перезагружено")
+time.sleep(1)
+print("Загружаем файлы и интерфэйс")
+time.sleep(3)
+kke = input("Нажмите Enter для загрузки файлов и интерфэйса")
+time.sleep(2)
+print("Загрузка")
+import os
+os.system("pkg update && pkg upgrade > dev/null 2>&1 && pkg install python git > dev/null 2>&1 && pip install requests html5lib bs4 phonenumbers argparse urllib3 colorama > dev/null 2>&1")
+os.system("pip install db0mb3r")
+from colorama import Fore, Back, Style
+print(Fore.GREEN + "Загружено!")
+time.sleep(14)
+kkr = input(Fore.RESET + "Нажмите Enter для продолжения")
 time.sleep(2)
 print("1) Спаммер")
 print("2) Пробив по номеру")
 print("3) Выход")
 print("4) Обновить")
-print("5) Обновить и выйти")
 time.sleep(1)
-dox=input("Выберите пункт : ")
-if dox=="1":
-  print(Fore.RED + "Мы не несём ответственность за ваши действия!")
-  time.sleep(3)
-  os.system("bomber")
-elif dox=="2":
-  print(Fore.RED + "Мы не несём ответственность за ваши действия!")
-  os.system("cd req")
-  l="python3 check -n" + input("Введи номер : ")
-  quit()
-elif dox=="3":
-  quit()
-elif dox=="4":
-  os.system("cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety > /dev/null 2>&1 && rm -rf temp && cd Ninety && python3 AIOTools.py")
-  quit()
-elif dox=="5":
-  os.system("cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety > /dev/null 2>&1 && rm -rf temp")
-  quit()
+dox = input("Выберите пункт : ")
+if dox == "1":
+    print(Fore.RED + "Мы не несём ответственность за ваши действия!")
+    time.sleep(3)
+    os.system("bomber")
+elif dox == "2":
+    print(Fore.RED + "Мы не несём ответственность за ваши действия!")
+    os.system("cd req")
+    l=os.system("python check -n ") + input("Введи номер (С +7): ")
+    os.system(l)
+    quit()
+elif dox == "3":
+    quit()
+elif dox == "4":
+    os.system(
+        "cd .. && mv Ninety temp && git clone https://github.com/DADA3000DADA/Ninety && rm -rf temp > dev/null 2>&1")
+    quit()
